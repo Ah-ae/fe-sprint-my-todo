ce04bab31ee1af26fc1730ea28891e59f01f809a
Ah-ae <kimaae@gmail.com>
2023-02-09 16:04:28 +0900

    삭제 버튼 클릭시 모달 구현

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -63,12 +63,72 @@ const ButtonGroup = styled.div`
   }
 `;
 
+const ModalBackdrop = styled.div`
+  // * 화면 전체 차지
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.35);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+`;
+
+const ModalBox = styled.div.attrs((props) => ({
+  role: "dialog",
+}))`
+  padding: 2rem;
+  width: 360px;
+  border-radius: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    text-align: center;
+  }
+  p:last-of-type {
+    margin-top: 0.2rem;
+    color: darkgray;
+    font-weight: 600;
+  }
+  span {
+    font-weight: 700;
+  }
+
+  .selectBtnGroup {
+    width: 100%;
+    margin-top: 2rem;
+    display: flex;
+    justify-content: flex-end;
+  }
+  button {
+    padding: 0.5rem 0.6rem;
+    background-color: lightgrey;
+    border-radius: 0.2rem;
+    font-weight: 600;
+  }
+  .deleteBtn {
+    margin-left: 0.5rem;
+    background-color: #db3a29;
+    color: #fff;
+  }
+`;
+
 export default function Todo({ todo, todos, setTodos }) {
   const { id, text, done, important } = todo;
   const [notEditable, setNotEditable] = useState(true);
   const [inputValue, setInputValue] = useState(text);
   const inputEl = useRef(null);
   const { REACT_APP_SERVER_URL: URL } = process.env;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModal = () => {
+    setShowModal((cur) => !cur);
+  };
 
   const taskDone = () => {
     const target = todos.find((todo) => todo.id === id);
@@ -163,8 +223,24 @@ export default function Todo({ todo, todos, setTodos }) {
             onClick={handleImportant}
           ></img>
           <img src={edit} onClick={editTodo} alt="edit icon"></img>
-          <img src={trash} onClick={deleteTodo} alt="delete icon"></img>
+          <img src={trash} onClick={handleModal} alt="delete icon"></img>
         </ButtonGroup>
+        {showModal && (
+          <ModalBackdrop onClick={handleModal}>
+            <ModalBox onClick={(e) => e.stopPropagation()}>
+              <p>
+                "{text}"이/가 <span>영구적으로 삭제</span>됩니다.
+              </p>
+              <p>이 작업은 취소할 수 없습니다.</p>
+              <div className="selectBtnGroup">
+                <button onClick={handleModal}>취소</button>
+                <button onClick={deleteTodo} className="deleteBtn">
+                  작업 삭제
+                </button>
+              </div>
+            </ModalBox>
+          </ModalBackdrop>
+        )}
       </Container>
     </>
   );
