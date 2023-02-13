62ccbb81ac900f7ec91dcb5cfa574a8539581919
Ahae KIM <kimaae@gmail.com>
2023-02-13 23:02:50 +0900

    netlify cd 테스트

--- src/App.js
+++ src/App.js
@@ -9,7 +9,24 @@ import Important from "./components/pages/Important";
 
 function App() {
   const { REACT_APP_SERVER_URL: URL } = process.env;
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    {
+      text: "json 서버 적용",
+      done: true,
+      important: true,
+      createdAt: "2023-02-09T01:19:29.559Z",
+      updatedAt: "2023-02-09T06:13:32.785Z",
+      id: 1,
+    },
+    {
+      text: "수정/삭제 버튼 더 나은 UI 방안 찾기",
+      done: false,
+      important: false,
+      createdAt: "2023-02-09T01:20:04.221Z",
+      updatedAt: "2023-02-09T07:03:18.933Z",
+      id: 4,
+    },
+  ]);
 
   useEffect(() => {
     async function getData() {
